5dc97474f225388d60b701d8913351f6601912bd
MyNameIsNickU <nicholas.untrecht@mavs.uta.edu>
2021-03-24 18:24:24 -0500

    Mostly works...boundary cases are showing up

--- src/threading/substring.c
+++ src/threading/substring.c
@@ -5,7 +5,7 @@
 #include <sys/time.h>
 
 #define MAX 5000000
-#define NUM_THREADS 4
+#define NUM_THREADS 3
 
 int total = 0;
 int n1,n2;
@@ -57,13 +57,14 @@ int readf(char* filename)
 
 void * num_substring ( void * ptr )
 {
-    //pthread_mutex_lock( &mutex );
     int i,j,k;
     int count;
     int start, end;
 
     start = *(int*)ptr;
     end = start + divisions;
+
+    printf("Searching... start: %d\tend: %d\n", start, end);
 
     for (i = start; i <= end; i++)
     {
@@ -79,16 +80,17 @@ void * num_substring ( void * ptr )
                 count++;
             }
             if (count==n2)
-                total++; /*find a substring in this step*/
+            {
+              total++; /*find a substring in this step*/
+            }
          }
     }
-    //pthread_mutex_unlock( &mutex );
     return NULL;
 }
 
 int main(int argc, char *argv[])
 {
-    int count;
+    int count, i;
 
     if( argc < 2 )
     {
@@ -107,13 +109,14 @@ int main(int argc, char *argv[])
     pthread_t substring_thread[NUM_THREADS];
 
     divisions = n1/NUM_THREADS;
-    int thread_start;
 
-    int i ;
+    int thread_start[NUM_THREADS];
+    for(i = 0; i < NUM_THREADS; i++)
+      thread_start[i] = divisions * i;
+
     for(i = 0; i < NUM_THREADS; i++)
     {
-      thread_start = divisions * i;
-      if( pthread_create(&substring_thread[i], NULL, num_substring, (void*) &thread_start ) )
+      if( pthread_create(&substring_thread[i], NULL, num_substring, (void*) &thread_start[i] ) )
       {
         perror("Thread creation error: ");
         exit( EXIT_FAILURE );
