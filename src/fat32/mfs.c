beda138750ce844e6d2245e85fb4d92e0e8b5486
MyNameIsNickU <nicholas.untrecht@mavs.uta.edu>
2021-04-12 20:19:44 -0500

    Implemented 'open'

--- src/fat32/mfs.c
+++ src/fat32/mfs.c
@@ -1,17 +1,17 @@
 // The MIT License (MIT)
-// 
-// Copyright (c) 2020 Trevor Bakker 
-// 
+//
+// Copyright (c) 2020 Trevor Bakker
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,11 +24,9 @@
 
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
-#include <signal.h>
 
 #define MAX_NUM_ARGUMENTS 3
 
@@ -38,6 +36,30 @@
                                 // will separate the tokens on our command line
 
 #define MAX_COMMAND_SIZE 255    // The maximum command-line size
+
+
+FILE *fp;
+
+int fat_open( char * filename )
+{
+  if( fp != NULL )
+  {
+    printf("Error: File system image already open.\n");
+    return 0;
+  }
+
+  fp = fopen( filename, "r" );
+  if( fp == NULL )
+  {
+    printf("Error: File system image not found.\n");
+    return -1;
+  }
+  else
+  {
+   printf("Image opened successfully.\n");
+   return 1;
+  }
+}
 
 
 int main()
@@ -60,13 +82,13 @@ int main()
     /* Parse input */
     char *token[MAX_NUM_ARGUMENTS];
 
-    int   token_count = 0;                                 
-                                                           
+    int   token_count = 0;
+
     // Pointer to point to the token
     // parsed by strsep
-    char *arg_ptr;                                         
-                                                           
-    char *working_str  = strdup( cmd_str );                
+    char *arg_ptr;
+
+    char *working_str  = strdup( cmd_str );
 
     // we are going to move the working_str pointer so
     // keep track of its original value so we can deallocate
@@ -74,7 +96,7 @@ int main()
     char *working_root = working_str;
 
     // Tokenize the input stringswith whitespace used as the delimiter
-    while ( ( (arg_ptr = strsep(&working_str, WHITESPACE ) ) != NULL) && 
+    while ( ( (arg_ptr = strsep(&working_str, WHITESPACE ) ) != NULL) &&
               (token_count<MAX_NUM_ARGUMENTS))
     {
       token[token_count] = strndup( arg_ptr, MAX_COMMAND_SIZE );
@@ -85,16 +107,25 @@ int main()
         token_count++;
     }
 
+
+
+
     // Now print the tokenized input as a debug check
     // \TODO Remove this code and replace with your FAT32 functionality
 
     int token_index  = 0;
-    for( token_index = 0; token_index < token_count; token_index ++ ) 
+    for( token_index = 0; token_index < token_count; token_index ++ )
     {
-      printf("token[%d] = %s\n", token_index, token[token_index] );  
+      printf("token[%d] = %s\n", token_index, token[token_index] );
     }
 
     free( working_root );
+
+    if( !strcmp( token[0], "open" ) )
+    {
+      fat_open( token[1] );
+    }
+
 
   }
   return 0;
